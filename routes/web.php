aa6a2d0eb5c99d5200149242ec0a2421b675164b
Gabriel fernandes <Gabrielfernandesfotografias@outlook.com>
2022-03-04 14:35:39 -0300

    update views & routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/gabriel', function () {
+    return view('gabriel');
+});
+
+Route::get('/kaio', function () {
+    return view('kaio');
+});
+
